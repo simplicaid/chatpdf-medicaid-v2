da7ff8a7f17414434d56fd2c88fbaa02c8d0e2bb
vtjong <vyt3@cornell.edu>
2024-02-03 08:33:53 -0500

    frontend UI fix, questionnaire

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -5,6 +5,7 @@ import { useChat } from "ai/react";
 import { Button } from "./ui/button";
 import { Send } from "lucide-react";
 import MessageList from "./MessageList";
+import IntakeQuestionnaire from "./IntakeQuestionnaire";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Message } from "ai";
@@ -12,6 +13,19 @@ import { Message } from "ai";
 type Props = { chatId: number };
 
 const ChatComponent = ({ chatId }: Props) => {
+  // TODO: for testing, set questionnaire state to true; later set to false
+  const [isQuestionnaireCompleted, setIsQuestionnaireCompleted] = React.useState(true);
+  const [question, setQuestion] = React.useState(0);
+  const [verify, setVerify] =  React.useState(false);
+
+  const increment = () => {
+    setQuestion(question + 1);
+  };
+
+  const decrement = () => {
+    setQuestion(question - 1);
+  };
+  
   const { data, isLoading } = useQuery({
     queryKey: ["chat", chatId],
     queryFn: async () => {
@@ -22,13 +36,23 @@ const ChatComponent = ({ chatId }: Props) => {
     },
   });
 
+  const initialAgentMessage: Message = {
+    id: '0',
+    content: `Hello! I am Simplicaid, and I'm here to guide you through the Medicaid application process.
+    We'll go through a series of questions to ensure we gather all the necessary information for your application,
+    and I'll try my best to fill out the Medicaid form for you. Please answer them to the best of your ability. Let's get started!`,
+    role: 'system',
+  };
+
+  // TODO: write to db so we can maintain messagse
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat",
     body: {
       chatId,
     },
-    initialMessages: data || [],
+    initialMessages: [initialAgentMessage],
   });
+
   React.useEffect(() => {
     const messageContainer = document.getElementById("message-container");
     if (messageContainer) {
@@ -38,31 +62,42 @@ const ChatComponent = ({ chatId }: Props) => {
       });
     }
   }, [messages]);
+
+    // Render IntakeQuestionnaire
+    if (!isQuestionnaireCompleted) {
+      return (
+        <IntakeQuestionnaire
+          onComplete={() => setIsQuestionnaireCompleted(true)}
+        />
+      );
+    }  
+  
+  // Render chatbot
   return (
-    <div
-      className="relative max-h-screen overflow-scroll"
-      id="message-container"
-    >
+    <div className="flex flex-col h-screen">
       {/* header */}
-      <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
-        <h3 className="text-xl font-bold">Chat</h3>
+      <div className="p-2 bg-white">
+        <h3 className="text-2xl font-bold pt-4 pl-4">Chat</h3>
       </div>
 
       {/* message list */}
-      <MessageList messages={messages} isLoading={isLoading} />
+      <div className="flex-grow overflow-auto pl-3" id="message-container"> 
+        <MessageList messages={messages} isLoading={isLoading} />
+      </div>
 
+      {/* input form */}
       <form
         onSubmit={handleSubmit}
-        className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white"
+        className="flex-none px-2 py-4 bg-white"
       >
-        <div className="flex">
+        <div className="flex pl-3 pb-2">
           <Input
             value={input}
             onChange={handleInputChange}
-            placeholder="Ask any question..."
+            placeholder="Enter..."
             className="w-full"
           />
-          <Button className="bg-blue-600 ml-2">
+          <Button type="submit" className="bg-blue-600 ml-2">
             <Send className="h-4 w-4" />
           </Button>
         </div>
--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -35,6 +35,7 @@ export async function loadS3IntoPinecone(fileKey: string) {
 
   // 2. split and segment the pdf
   const documents = await Promise.all(pages.map(prepareDocument));
+  console.log("pdf segmented successfully")
 
   // 3. vectorise and embed individual documents
   const vectors = await Promise.all(documents.flat().map(embedDocument));
